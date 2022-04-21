e0795045c32d8e8e5fe51164dacfe7a905b9200c
Daniyal Shahid <43936046+dani1560@users.noreply.github.com>
2022-04-21 13:10:40 +0500

    Update AssetsBundleBroser.cs

--- Assets Bundle Browser/Assets/AssetsBundleBroser.cs
+++ Assets Bundle Browser/Assets/AssetsBundleBroser.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class AssetsBundleBroser : MonoBehaviour
 {
     IEnumerator Start()
     {
         string url = "E:\\assetsBundle\\enemy";
-        WWW webReq = new WWW("file:///" + url);
+        // WWW webReq = new WWW("file:///" + url);
+        //WWW replace with web request..
+        UnityWebRequest webReq = UnityWebRequestAssetBundle.GetAssetBundle(url);
 
         while (!webReq.isDone) {
-            yield return null;
+            yield return webReq.SendWebRequest();
 
-            AssetBundle assObj = webReq.assetBundle;
+            AssetBundle assObj = DownloadHandlerAssetBundle.GetContent(webReq);
 
             if(assObj != null)
             {
